7adfc5005fb70a459cb6a7966a9ce47e4bdb5494
Solomon sokoya <solomonsokoya@ool-ad020098.dyn.optonline.net>
2018-07-02 12:25:15 -0400

    created store and sent a dispatched

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import {createStore} from 'redux';
 import counter from './reducers';
 
+const store = createStore(counter);
+console.log(store.getState())
+
+store.dispatch({type: 'INCREMENT'});
+console.log(store.getState())
 
 class App extends Component {
   render() {
