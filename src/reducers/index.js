7f386cd18c39c9161dde79b873589dad8437db2c
Solomon sokoya <solomonsokoya@ool-ad020098.dyn.optonline.net>
2018-07-10 15:18:28 -0400

    m

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,31 +1,37 @@
-const counter = (state = 0, action) =>{
-  switch(action.type){
+const counter = (state = 0, action) => {
+  switch (action.type) {
     case 'INCREMENT':
       return state + 1;
     case 'DECREMENT':
       return state - 1;
     default:
-    return state;
+      return state;
   }
 }
-
-const todos = (state = [], action) =>{
-  switch(action.type){
-    case 'ADD_TODO':
-    return [...state,
-      {
-        id: action.id,
-        text: action.text,
-        completed: false
-      }
-
-  ];
-
-  default:
-  return state;
-}
-};
-
-
+// const todos = (state = [], action) => {
+//   switch (action.type) {
+//     case 'ADD_TODO':
+//       return [
+//         ...state, {
+//           id: action.id,
+//           text: action.text,
+//           completed: false
+//         }
+//       ];
+//     case 'TOGGLE_TODO':
+//       return state.map(todo => {
+//         if (todo.id !== action.id) {
+//           return todo;
+//         }
+//         return {
+//           ...todo,
+//           completed: !todo.completed
+//         }
+//       })
+//
+//     default:
+//       return state;
+//   }
+// };
 
 export default counter;
